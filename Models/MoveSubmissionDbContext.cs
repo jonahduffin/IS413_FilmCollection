d09635db3a63607f9c68bbd84a138611094fa35d
Jonah Duffin <jonahfduffin@gmail.com>
2021-03-24 11:32:34 -0600

    Added MovieSubmissionDbContext, connection string, and enabled services in startup.cs. About to try and migrate the database

---
+++ Models/MoveSubmissionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace joel_hilton_film_collection.Models
+{
+    public class MovieSubmissionDbContext : DbContext
+    {
+        public MovieSubmissionDbContext (DbContextOptions<MovieSubmissionDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<MovieSubmissionResponse> Submissions { get; set; }
+    }
+}
